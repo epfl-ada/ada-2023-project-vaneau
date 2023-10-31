16b208cc1c9a28313c46dc90676e863f3bda49dc
erwanndebelloy <erwanndebelloy1@gmail.com>
2023-10-31 18:41:27 +0100

    data processing

--- Helpers.py
+++ Helpers.py
@@ -1,37 +1,54 @@
 import pandas as pd
+import numpy as np
 import json
-import re
 
 DATA_FOLDER = "MovieSummaries/"
 
-MOVIE_COLUMNS_TO_CLEAN = ['Freebase movie ID','Movie languages','Movie genres','Movie countries']
+MOVIE_COLUMNS_TO_CLEAN = ['Fb_movie_id','Movie languages','Movie genres','Movie countries']
 
-CHARACTER_COLUMNS_TO_CLEAN = ["Freebase movie ID", "Actor ethnicity (Freebase ID)", "Freebase character ID",
-                               "Freebase actor ID","Freebase character/actor map ID"]
+CHARACTER_COLUMNS_TO_CLEAN = ["Fb_movie_id", "Fb_actor_ethnicity_id", "Fb_char_id",
+                               "Fb_actor_id","Freebase character/actor map ID"]
 
-NAME_COLUMNS_TO_CLEAN = ["ID"]
+NAME_COLUMNS_TO_CLEAN = ["Fb_char_actor_id"]
 
 def load_data(file_path):
     if (file_path == "movie.metadata.tsv"):
         df = pd.read_csv(DATA_FOLDER + "movie.metadata.tsv", sep='\t', header=None,
-                 names=["Wikipedia movie ID", "Freebase movie ID", "Movie name",
-                        "Movie release date", "Movie box office revenue",
+                 names=["Wiki_movie_id", "Fb_movie_id", "Movie name",
+                        "release_date", "Movie box office revenue",
                         "Movie runtime", "Movie languages", "Movie countries", "Movie genres"])
         # Apply the clean_column function to the specified columns
         df[MOVIE_COLUMNS_TO_CLEAN] =df[MOVIE_COLUMNS_TO_CLEAN].apply(clean_column)
+
+        # Use str.extract() to extract the year from the 'release_date' column
+        df['release_date'] = df['release_date'].str.extract(r'(\d{4})')
+
+        #Remove language in the 'Movie languages' column as it appears in every row
+        df['Movie languages'] = df['Movie languages'].str.replace('language', '', case=False)
+
+        df['Movie runtime'] = df['Movie runtime'].apply(lambda x: np.nan if x > 15000 else x)
+
     elif (file_path == "plot_summaries.txt"):
-        df = pd.read_csv(DATA_FOLDER + "plot_summaries.txt", sep='\t', header=None, names=["Wikipedia movie ID", "Summary"])
+        df = pd.read_csv(DATA_FOLDER + "plot_summaries.txt", sep='\t', header=None, names=["Wiki_movie_id", "Summary"])
+
     elif (file_path == "character.metadata.tsv"):
         df = pd.read_csv(DATA_FOLDER+"character.metadata.tsv", sep='\t', header=None,
-                 names=["Wikipedia movie ID", "Freebase movie ID", "Movie release date",
-                        "Character name", "Actor date of birth", "Actor gender",
-                        "Actor height (in meters)", "Actor ethnicity (Freebase ID)",
-                        "Actor name", "Actor age at movie release", "Freebase character/actor map ID",
-                        "Freebase character ID", "Freebase actor ID"])
+                 names=["Wiki_movie_id", "Fb_movie_id", "release_date",
+                        "Character", "Actor date of birth", "Actor gender",
+                        "Actor_height", "Fb_actor_ethnicity_id",
+                        "Actor_name", "Actor_age", "Freebase character/actor map ID",
+                        "Fb_char_id", "Fb_actor_id"])
         # Apply the clean_column function to the specified columns
         df[CHARACTER_COLUMNS_TO_CLEAN] =df[CHARACTER_COLUMNS_TO_CLEAN].apply(clean_column)
+
+        # Use str.extract() to extract the year from the 'release_date' column
+        df['release_date'] = df['release_date'].str.extract(r'(\d{4})')
+
+        df['Actor_age'] = df['Actor_age'].apply(lambda x: np.nan if x < 0 else x)
+        df['Actor_height'] = df['Actor_height'].apply(lambda x: np.nan if x > 2.5 else x)
+
     elif (file_path == "name.clusters.txt"):
-        df = pd.read_csv(DATA_FOLDER+"name.clusters.txt", sep='\t', header=None, names=['Name', 'ID'])
+        df = pd.read_csv(DATA_FOLDER+"name.clusters.txt", sep='\t', header=None, names=['Name', 'Fb_char_actor_id'])
         # Apply the clean_column function to the specified columns
         df[NAME_COLUMNS_TO_CLEAN] =df[NAME_COLUMNS_TO_CLEAN].apply(clean_column)
     elif (file_path == "tvtropes.clusters.txt"):
@@ -40,7 +57,7 @@ def load_data(file_path):
         # Extract specific JSON values into separate columns
         df['Char'] = df['Values'].apply(lambda x: extract_json_values(x, 'char'))
         df['Movie'] = df['Values'].apply(lambda x: extract_json_values(x, 'movie'))
-        df['ID'] = df['Values'].apply(lambda x: extract_json_values(x, 'id'))
+        df['Fb_char_actor_id'] = df['Values'].apply(lambda x: extract_json_values(x, 'id'))
         df['Actor'] = df['Values'].apply(lambda x: extract_json_values(x, 'actor'))
 
         # Apply the clean_column function to the specified columns
@@ -68,3 +85,4 @@ def extract_json_values(json_str, key):
         return data.get(key, None)
     except json.JSONDecodeError:
         return None
+

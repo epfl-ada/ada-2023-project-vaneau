b16efcb0d7480898094800356a3546807a98cc31
Broesmelii <rookhammock@pm.me>
2023-11-28 23:57:02 +0100

    minor update

--- src/hollywood_viz/hollywood.py
+++
@@ -1,7 +0,0 @@
-
-def isInList(entries_to_check: list, valid_entries: list) -> bool:
-    if entries_to_check:
-        for studio in entries_to_check:
-            if studio in valid_entries:
-                return True
-    return False
